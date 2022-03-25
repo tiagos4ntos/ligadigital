0f8e8561b1d6bcfd924f9992848ba0a33cbb2386
Leopoldo Barbato <leopoldo.barbato@gmail.com>
2022-03-25 16:05:55 -0300

    Definição dos atributos e métodos da classe, e instanciação de dois objetos filho.

--- poo/leopoldo/objanimaisfilhos.js
+++ poo/leopoldo/objanimaisfilhos.js
@@ -1,10 +1,15 @@
 const animais = require('./objanimaispai')
 
 
-let onca = new animais("Chordata", "Mammalia", "Carnívoro", "Felídeos", "Américas", 90);
-let capivara = new animais("Chordata", "Mammalia", "Rodentia", "Hydrochoeridae", 30);
+let onca = new animais("Chordata", "Mammalia", "Carnívoro", "Felídeos", "Américas", 60);
+let capivara = new animais("Chordata", "Mammalia", "Rodentia", "Hydrochoeridae", "Américas", 30);
 
 
-capivara.inicio(30);
+capivara.inicio(60);
 capivara.sentirSede();
-capivara.deslocamento(35);
+capivara.deslocamento(240);
+capivara.beberAgua();
+onca.inicio(480);
+onca.sentirFome();
+onca.deslocamento(capivara.posicao);
+onca.fim();
--- poo/leopoldo/objanimaispai.js
+++ poo/leopoldo/objanimaispai.js
@@ -19,7 +19,107 @@ class animais {
         console.log(JSON.stringify(this)); 
     }
     
-    // defineDirecao(posfinal) {
+    inicio(posinit) {
+        console.log("Em algum lugar das " + this.habitat + ", uma...");
+        this.descrever();
+        console.log("... estava ali, parada.");
+        this.posicao += posinit;
+        console.log("(Posição: " + this.posicao + ")");
+    }
+
+    sentirSede() {
+        this.sede = true;
+        console.log("Então a ")
+        this.descrever();
+        console.log(" sentiu muita sede.");
+        console.log("(Sede: " + this.sede + ")")
+    }
+
+    sentirFome() {
+        this.fome = true;
+        console.log("Então a ")
+        this.descrever();
+        console.log(" sentiu muita fome.");
+        console.log("(Fome: " + this.fome + ")")
+    }
+
+    beberAgua() {
+        this.sede = false;
+        console.log("Rapidamente a ")
+        this.descrever();
+        console.log(" bebeu àgua até saciar sua sede.");
+        console.log("(Sede: " + this.sede + ")")
+    }
+
+    deslocamento(posfinal) {      
+        if (this.posicao < posfinal) {
+            let distancia = posfinal-this.posicao
+            console.log("Para isso teria que andar " + distancia + " metros para a direita, até a Posição: " + posfinal + ".")
+            while (this.posicao < posfinal) { 
+                this.posicao += this.aceleracao;
+                switch (posfinal-this.posicao) {
+                    case (0):
+                        console.log("Chegou!");
+                        console.log("(Posição: " + this.posicao + ")");
+                        break;
+                    case (distancia*0.2):
+                        console.log("Quase chegando...");
+                        console.log("(Posição: " + this.posicao + ")");
+                        break;
+                    case (distancia*0.5):
+                        console.log("Passou da metade do caminho...");
+                        console.log("(Posição: " + this.posicao + ")");
+                        break;
+                    case (distancia-this.aceleracao):
+                        console.log("E lá se foi...");
+                        console.log("(Posição: " + this.posicao + ")");
+                        break;                
+                    default:
+                        break;
+                }                     
+            }
+        } else if (this.posicao > posfinal) {
+            let distancia = this.posicao-posfinal
+            console.log("Para isso teria que andar " + distancia + " metros para a esquerda, até a Posição: " + posfinal + ".")
+            while (this.posicao > posfinal) { 
+                this.posicao -= this.aceleracao;
+                switch (this.posicao-posfinal) {
+                    case (0):
+                        console.log("Chegou!");
+                        console.log("(Posição: " + this.posicao + ")");
+                        break;
+                    case (distancia*0.2):
+                        console.log("Quase chegando...");
+                        console.log("(Posição: " + this.posicao + ")");
+                        break;
+                    case (distancia*0.5):
+                        console.log("Passou da metade do caminho...");
+                        console.log("(Posição: " + this.posicao + ")");
+                        break;
+                    case (distancia-this.aceleracao):
+                        console.log("E lá se foi...");
+                        console.log("(Posição: " + this.posicao + ")");
+                        break;                
+                    default:
+                        break;
+                }      
+            }
+        } else {
+            console.log("Para isso não precisaria andar, porque já estava na posição: " + posfinal + ".");
+        }
+    }
+
+    fim(){
+        console.log("Neste momento a onça olhou para a capivara e disse: '-Tenho foooome!'");
+        console.log("A capivara imediatemente respondeu: '-#VemSerDev! Só tem fera!'");
+        console.log("-------------------------- FIM! --------------------------");
+    }
+}
+
+module.exports=animais
+
+//Primeira tentativa de simular um vetor de movimento [X, Y] para executar um pulo (parábola)
+// defineDirecao(posfinal) {
     //     if (this.posicao < posfinal) {
     //         this.direcao = +1;
     //         console.log(">>===>>");
@@ -38,38 +138,3 @@ class animais {
     //     }
      
     // }
-
-    inicio(posinit) {
-        console.log("Uma... ");
-        this.descrever();
-        console.log("... estava em seu lugar.");
-        this.posicao += posinit;
-        console.log("(Posição: " + (this.posicao) + ")");
-    }
-
-    sentirSede() {
-        this.sede = true;
-        console.log("Então a ") + this.descrever() + console.log(" sentiu muita sede");
-    }
-
-    deslocamento(posfinal) {
-        console.log("Para isso teria que andar " + posfinal + " metros")
-        if (this.posicao < posfinal) {
-            while (this.posicao < posfinal) { 
-                this.posicao += 1;
-                console.log(this.posicao);      
-            }
-        } else if (this.posicao > posfinal) {
-            while (this.posicao > posfinal) { 
-                this.posicao -= 1;
-                console.log(this.posicao);      
-            }
-        }
-        
-
-    }
-
-
-}
-
-module.exports=animais
